92dbaa1839903b05a6d8ea49dfa009a4b27fc995
personalmac <aalleexsh2011@yandex.ru>
2022-09-06 11:11:31 +0300

    start creating actual VIEW templates to be used by Control and Model with include()

---
+++ templates/footer.php
@@ -0,0 +1,10 @@
+<footer class="text-center text-muted mt-2">
+      <div class="p-3" style="background-color: rgba(0, 0, 0, 0.05)">
+        © 2022 Copyright:
+        <a class="text-reset fw-bold" href="https://github.com/alex2011576"
+          >Aleksei Shatalov</a
+        >
+      </div>
+    </footer>
+  </body>
+</html>
