97ad2b28f0922c1690f5dd17098d59fb3be7a5f2
personalmac <aalleexsh2011@yandex.ru>
2022-09-22 00:09:48 +0300

    flash.php error->danger for bootstrap

--- src/libs/flash.php
+++ src/libs/flash.php
@@ -2,7 +2,7 @@
 
 const FLASH = 'FLASH_MESSAGES';
 
-const FLASH_ERROR = 'error';
+const FLASH_ERROR = 'danger';
 const FLASH_WARNING = 'warning';
 const FLASH_INFO = 'info';
 const FLASH_SUCCESS = 'success';
